f5089eb65eb881a828594ae84127e0a5ee641d2c
ims <ims21@users.sourceforge.net>
2012-11-18 12:30:01 +0100

    divide mem info into 2 columns

--- setup.py
+++ setup.py
@@ -3,10 +3,10 @@ import setup_translate
 
 pkg = 'Extensions.CacheFlush'
 setup (name = 'enigma2-plugin-extensions-cacheflush',
-       version = '1.10',
+       version = '1.16',
        description = 'periodicaly flush box cache',
        packages = [pkg],
        package_dir = {pkg: 'plugin'},
-       package_data = {pkg: ['*/*.png', 'locale/*/LC_MESSAGES/*.mo']},
+       package_data = {pkg: ['*.png', 'locale/*/LC_MESSAGES/*.mo']},
        cmdclass = setup_translate.cmdclass, # for translation
       )
